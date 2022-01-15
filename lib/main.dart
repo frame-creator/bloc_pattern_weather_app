0ed1caa16ca79a9bcce9b95024018227f0896522
code-framer <journeyofman@naver.com>
2022-01-15 21:51:04 +0900

    json 파싱 데이터 ui 샘플 렌더링

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:bloc_pattern_weather_app/model/weather_model.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:bloc_pattern_weather_app/main.dart';
+import 'dart:convert' as convert;
 
 void main() {
   runApp(MyApp());
@@ -10,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -27,19 +31,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  WeatherModel _weather;
+
+  Future<WeatherModel> fetchData() async {
+    var address = 'http://api.airvisual.com/v2/nearest_city?';
+    final url = Uri.parse(address);
+    var response = await http.get(url);
+    var jsonResponse = convert.jsonDecode(response.body);
+    var code = jsonResponse['status'];
+
+    WeatherModel weather = WeatherModel.fromJson(jsonResponse);
+    return weather;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData().then((WeatherModel) {
+      setState(() {
+        _weather = WeatherModel;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '날씨앱',
+      body: _weather == null
+          ? CircularProgressIndicator()
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    '${_weather.data.current.pollution.aqius}',
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
     );
   }
 }
