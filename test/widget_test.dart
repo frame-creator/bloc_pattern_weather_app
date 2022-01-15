347624f67c94ce4717426239bcf8232a8c53387d
code-framer <journeyofman@naver.com>
2022-01-15 20:54:59 +0900

    테스트위젯 json 파싱 테스트

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,9 @@
+import 'package:bloc_pattern_weather_app/model/weather_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:bloc_pattern_weather_app/main.dart';
+import 'dart:convert' as convert;
 
 void main() {
   test('api 테스트', () async {
@@ -9,5 +11,13 @@ void main() {
     final url = Uri.parse(address);
     var response = await http.get(url);
     expect(response.statusCode, 200);
+
+    var jsonResponse = convert.jsonDecode(response.body);
+    var code = jsonResponse['status'];
+
+    expect(code, 'success');
+
+    WeatherModel result = WeatherModel.fromJson(jsonResponse);
+    expect(result.status, 'success');
   });
 }
